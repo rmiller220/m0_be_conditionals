0704fb73bfef2912235db1f1db4da130e56a0d43
Reilly Miller <reillyjmiller31@gmail.com>
2023-01-04 13:30:38 -0700

    answer6

--- comparisons.rb
+++ comparisons.rb
@@ -43,6 +43,7 @@ p "is number_students greater than or equal to 21?", number_students >= 21
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
 # this should print: true
+p "is number_students less than or equal to 20?", number_students <= 21
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
 # this should print: true
