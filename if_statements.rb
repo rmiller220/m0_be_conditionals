f021ec0d196592ea1a5424c53555b81d83bf68fe
Reilly Miller <reillyjmiller31@gmail.com>
2023-01-04 14:40:37 -0700

    test the next example

--- if_statements.rb
+++ if_statements.rb
@@ -63,8 +63,8 @@ end
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour = 1
-has_sauce = true
+cups_of_flour = 5
+has_sauce = false
 
 if cups_of_flour < 2 && has_sauce
 p "I cannot make pizza"
